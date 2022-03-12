0e78e3bc9858345a1a3ed7decc54beec4b546817
Mathias <mat.micheli99@gmail.com>
2022-03-12 17:58:35 +0100

    feat (backend): add validation system

--- backend/src/Entity/PhoneBook.php
+++ backend/src/Entity/PhoneBook.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\PhoneBookRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PhoneBookRepository::class)]
 class PhoneBook
@@ -14,13 +15,20 @@ class PhoneBook
     private $id;
 
     #[ORM\Column(type: 'string', length: 255)]
-    private $firstname;
+    #[Assert\NotBlank]
+    private string $firstname;
 
     #[ORM\Column(type: 'string', length: 255)]
-    private $lastname;
+    #[Assert\NotBlank]
+    private string $lastname;
 
     #[ORM\Column(type: 'string', length: 255)]
-    private $phone_number;
+    #[Assert\NotBlank]
+    #[Assert\Regex(
+        pattern: '/^(\+)39[[:blank:]][0-9]{2}[[:blank:]][0-9]{7}$/i',
+        message: 'Your phone is not correct',
+    )]
+    private string $phone_number;
 
     public function getId(): ?int
     {
